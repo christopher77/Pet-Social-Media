12712e8a574360672100bf678375a98f1794746a
christopher <christoph.roa@gmail.com>
2021-05-19 14:41:37 -0500

    Get posts data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-import "./App.css";
+const BASE_URL = "https://dummyapi.io/data/api";
+const APP_ID = "60a555eb48e7e45fc7562a40";
 
-function App() {
-	return <div>PET-SOCIAL-MEDIA</div>;
-}
+const App = () => {
+	const [loading, setLoading] = useState(false);
+	const [data, setData] = useState(null);
+
+	useEffect(() => {
+		setLoading(true);
+		getData();
+	}, []);
+
+	const getData = async () => {
+		try {
+			const data = await axios.get(`${BASE_URL}/post`, {
+				headers: { "app-id": APP_ID },
+			});
+			setData(data.data.data);
+			console.log("dataGet=>", data.data.data[0]);
+		} catch (error) {
+			console.log(error);
+		}
+		setLoading(false);
+	};
+
+	return (
+		<div>
+			{loading && "Loading..."}
+			{JSON.stringify(data)}
+		</div>
+	);
+};
 
 export default App;
